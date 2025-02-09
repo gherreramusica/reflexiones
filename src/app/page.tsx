8129cbb3851a04b326a461b8f1c042f89757cb86
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2025-02-09 17:02:54 -0500

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Poppins } from "next/font/google";
+import Link from "next/link";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <div className={`grid place-content-center h-[100vh] bg-[linear-gradient(to_bottom_right,#e5e7eb_1px,transparent_1px),linear-gradient(to_bottom_left,#e5e7eb_1px,transparent_1px)] bg-[size:10px_10px] ${poppins.variable}`}>
       <main className="md:text-8xl text-5xl grid place-content-center font-poppins">
-        <h1>REFLEXIONES</h1>
+         <Link href="/home" ><h1>REFLEXIONES</h1></Link>
         <p className="text-2xl">El poder de la palabra...</p>
       </main>
     </div>
