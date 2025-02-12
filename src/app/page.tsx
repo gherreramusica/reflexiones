af8902b16bbeab09726965dbf6fe45956035f894
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2025-02-12 13:02:23 -0500

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ const poppins = Poppins({
 export default function Home() {
   return (
     <div className={`grid place-content-center h-[100vh] bg-[linear-gradient(to_bottom_right,#e5e7eb_1px,transparent_1px),linear-gradient(to_bottom_left,#e5e7eb_1px,transparent_1px)] bg-[size:10px_10px] ${poppins.variable}`}>
-      <main className="md:text-8xl text-5xl grid place-content-center font-poppins">
-         <Link href="/home" ><div className="relative"><h1 className="font-bold before:content-[''] before:absolute before:right-[0] before:bottom-[-25px] before:text-blue-500 before:w-[100px] before:h-[20px] before:bg-green-400">REFLEXIONES</h1></div></Link>
+      <main className="text-[200px]   grid place-content-center font-poppins">
+         <Link href="/home" ><div className="relative"><h1 className="font-bold before:content-[''] before:absolute before:right-[5] before:bottom-[10%] before:text-blue-500  before:w-[40px] before:h-[40px]  before:bg-green-400">R</h1></div></Link>
 
       </main>
     </div>
