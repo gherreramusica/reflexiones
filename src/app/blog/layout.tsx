7e88e974919d7e6cbeb350171fa440e220ae73a7
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2025-02-11 21:59:25 -0500

    home => header sticky

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -9,7 +9,7 @@ export default function BlogLayout({ children }: LayoutProps) {
   return (
     <div className="h-screen flex flex-col ">
       {/* header */}
-      <header className="p-3 bg-white text-black text-center text-3xl font-bold border-b">
+      <header className="p-3 sticky top-0 z-[99] bg-white text-black text-center text-3xl font-bold border-b">
         <div className="relative w-fit m-auto">
           <Link href="/home">
             <h1 className="relative text-3xl font-bold before:content-[''] before:block before:absolute before:right-0 before:bottom-[-5px] before:w-[10px] before:h-[10px] before:bg-green-400 before:text-blue-500">
