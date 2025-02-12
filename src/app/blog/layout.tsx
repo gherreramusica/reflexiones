3ce69299a7e169d213f50588c2dde4a640135604
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2025-02-11 21:37:53 -0500

    updates titlew

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -10,12 +10,12 @@ export default function BlogLayout({ children }: LayoutProps) {
     <div className="h-screen flex flex-col ">
       {/* header */}
       <header className="p-3 bg-white text-black text-center text-3xl font-bold border-b">
-      <div className="relative w-fit m-auto">
-        <Link href="/home">
-          <h1 className="relative text-3xl font-bold before:content-[''] before:absolute before:right-[0] before:bottom-[-5] before:text-blue-500 before:w-[10px] before:h-[10px] before:bg-green-400">
-            R
-          </h1>
-        </Link>{" "}
+        <div className="relative w-fit m-auto">
+          <Link href="/home">
+            <h1 className="relative text-3xl font-bold before:content-[''] before:block before:absolute before:right-0 before:bottom-[-5px] before:w-[10px] before:h-[10px] before:bg-green-400 before:text-blue-500">
+              R
+            </h1>
+          </Link>{" "}
         </div>
       </header>
       <main>{children}</main>
--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -18,13 +18,13 @@ export default function HomeLayout({ children }: LayoutProps) {
       {/* header */}
       <header className="p-3 bg-white text-black text-center text-3xl font-bold border-b">
         <div className="relative w-fit m-auto">
-        <Link href="/home">
-          <h1 className="relative text-3xl font-bold before:content-[''] before:absolute before:right-[0] before:bottom-[-5] before:text-blue-500 before:w-[10px] before:h-[10px] before:bg-green-400">
-            R
-          </h1>
-        </Link>
+          <Link href="/home">
+            <h1 className="relative text-3xl font-bold before:content-[''] before:block before:absolute before:right-0 before:bottom-[-5px] before:w-[10px] before:h-[10px] before:bg-green-400 before:text-blue-500">
+              R
+            </h1>
+          </Link>
         </div>
- </header>
+      </header>
       <main>{children}</main>
     </div>
   );
