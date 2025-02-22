f1458860ae8037c3c00f0f6aa4463ede9837f105
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2025-02-21 22:20:06 -0500

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    appDir: false, // 🔹 Si usas Pages Router (`pages/`), desactiva App Router
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.externals = {
-        formidable: "commonjs formidable",
-        "mongodb-client-encryption": "commonjs mongodb-client-encryption",
-        kerberos: "commonjs kerberos",
-      };
-
-      // 🚀 Permite módulos de Node.js en Webpack 5
-      config.resolve.fallback = {
-        async_hooks: false, // 🔹 Evita error con "node:async_hooks"
-        crypto: false, // 🔹 Evita error con `crypto`
-        fs: false, // 🔹 Evita error con `fs`
-      };
-
-      // 🔹 Evita errores con `react-dom/server.edge`
-      config.resolve.alias = {
-        "react-dom/server": "react-dom/server.browser",
-      };
+      config.externals = { formidable: "commonjs formidable" };
     }
-
     return config;
   },
   images: {
