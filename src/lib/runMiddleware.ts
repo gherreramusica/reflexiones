fdc4e0af113b79cdc5b09d10a344e703fb546cc1
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2025-02-21 22:44:02 -0500

    updates build

--- src/lib/runMiddleware.ts
+++ src/lib/runMiddleware.ts
@@ -1,12 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
+type MiddlewareFunction = (req: NextApiRequest, res: NextApiResponse, next: (result?: unknown) => void) => void;
+
+export function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: MiddlewareFunction) {
+  return new Promise<void>((resolve, reject) => {
+    fn(req, res, (result?: unknown) => {
       if (result instanceof Error) {
         return reject(result);
       }
-      return resolve(result);
+      return resolve();
     });
   });
 }
